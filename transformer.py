b7e9c8253cd09107c94c9fbbaa379d790350039a
pedro <pe.santos91@gmail.com>
2025-10-21 12:04:53 +0200

    notes added

--- transformer.py
+++ transformer.py
@@ -1,3 +1,12 @@
 # database is here: https://drive.google.com/file/d/1SB1g8ectHyhG23g8FqiwKJApyvmK5Sc9/view?usp=sharing
 # paper is here https://arxiv.org/pdf/2203.10694
 
+"""
+FO and FA mimics the functionality of the original Transformer architecture as closely as possible while being
+optimized for fast autoregressive inference. Which means, that the object detection + self-attention is done, 
+but fully on the frequency domain using Fourier transforms, enabling the FAR model to be robust and efficient 
+to moving backgrounds or dynamic cameras.
+
+The FO (Fourier Object Disentanglement) separates the object from the background withput needing any bounding box
+FA captures long range space-time relationships at a very lower computational cost using FFT 
+"""
